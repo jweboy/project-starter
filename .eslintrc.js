c31ce736e72425bb92fadc612379d0f8a62e2cd6
jweboy <1259524459@qq.com>
2019-06-12 19:16:47 +0800

    :sparkles: Add env property in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   extends: ['airbnb-base', 'prettier'],
   plugins: ['prettier'],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
   parserOptions: {
     sourceType: 'module'
     // ecmaFeatures: {
